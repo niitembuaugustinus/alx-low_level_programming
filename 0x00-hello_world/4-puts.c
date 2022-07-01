e256f16fd52c1df509ace7c9754ce618d8d073b5
niitembuaugustinus <niitembuaugustinus@gmail.com>
2022-06-30 17:15:11 -0700

    final fix2/4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main - entry point
- *
- * Return: 0 after printing the function
- */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
