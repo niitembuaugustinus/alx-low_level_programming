bb8cc3b6e0dc5a8582220ca259bfebfeaa7eab6e
niitembuaugustinus <niitembuaugustinus@gmail.com>
2022-07-04 19:46:25 -0700

    advance1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,21 @@
 /**
  * main - entry point
  *
- * Return: always returns 0
+ * Return: always return 0
  */
 int main(void)
 {
 int i;
 int j;
-for (i = 10; i <= 19; i++)
+for (i = 10; i < 19; i++)
 {
-for (j - 10; j <= 19; j++)
+for (j = 10; j < 19; j++)
 {
 if ((j % 10) > (i % 10))
 {
-putchar((i % 10) + '0')
-putchar((j % 10) + '0')
-if (i != 18 || j !=19)
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i != 18 || j != 19)
 {
 putchar(',');
 putchar(' ');
@@ -24,6 +24,8 @@ putchar(' ');
 }
 }
 }
-putchar("\n");
+{
+putchar('\n');
+}
 return (0);
 }
