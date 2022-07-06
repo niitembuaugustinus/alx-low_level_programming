e2f30cbc2661150f295d4baf382134418ebf9074
niitembuaugustinus <niitembuaugustinus@gmail.com>
2022-07-06 15:55:21 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main-main block
+ * Description: A program that printd _putchar
+ * Returns: 0 Success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r'r);
+	_putchar('\n');
+
+	return (0);
+}
