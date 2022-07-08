b06daa0363778f3db9e6ac19bd98362cadc2e9a5
niitembuaugustinus <niitembuaugustinus@gmail.com>
2022-07-08 15:32:45 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isuppper - checkes upper case
+ * @c: print int
+ * Return: give 1 0r 0
+ */
+
+int main(void)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
