a885f3b6312ea53d7525f55dba4102e7a82cee18
niitembuaugustinus <niitembuaugustinus@gmail.com>
2022-07-12 14:14:52 -0700

    fix2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,17 @@
 
 /**
  * _strlen - function that returns the length of a string
+ *
  * @s: this it the input string
+ *
  * Return: Length of the string
  */
 
 int _strlen(char *s)
 {
 	int index;
+
 	for (index = 0; s[index] != '\0' ; index++);
+
 	return (index);
 }
